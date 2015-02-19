e487f6e6c964a90506b4803b78608ca117952ee9
Stef Busking <stef.busking@gmail.com>
2015-02-19 11:29:16 +0100

    Directive should be a function returning the definition object.

--- src/ui/createUiDocumentReferencePopoverDirective.js
+++ src/ui/createUiDocumentReferencePopoverDirective.js
@@ -27,43 +27,45 @@ define([
 		});
 	}
 
-	return {
-		restrict: 'E',
-		templateUrl: require.toUrl('fontoxml-references-document/ui/ui-document-reference-popover.html'),
-		scope: {},
-		require: '^uiReferencePopover',
-		link: function (scope, element, attrs, uiReferencePopoverController) {
-			// TODO: use a generic document edit operation here which has a step which determines the specific implementation
-			// according to the element being edited.
-			uiReferencePopoverController.setEditOperationName('xref-dita-edit');
+	return function createUiDocumentReferencePopoverDirective () {
+		return {
+			restrict: 'E',
+			templateUrl: require.toUrl('fontoxml-references-document/ui/ui-document-reference-popover.html'),
+			scope: {},
+			require: '^uiReferencePopover',
+			link: function (scope, element, attrs, uiReferencePopoverController) {
+				// TODO: use a generic document edit operation here which has a step which determines the specific implementation
+				// according to the element being edited.
+				uiReferencePopoverController.setEditOperationName('xref-dita-edit');
 
-			scope.uiDocumentReferencePopover = {
-				uiReferencePopover: uiReferencePopoverController,
+				scope.uiDocumentReferencePopover = {
+					uiReferencePopover: uiReferencePopoverController,
 
-				previewReference: function () {
-					resolveRemoteDocumentIdFromTarget(
-						uiReferencePopoverController.reference,
-						uiReferencePopoverController.nodeId)
-						.then(function(documentRemoteId) {
-							uiReferencePopoverController.hidePopover();
+					previewReference: function () {
+						resolveRemoteDocumentIdFromTarget(
+							uiReferencePopoverController.reference,
+							uiReferencePopoverController.nodeId)
+							.then(function(documentRemoteId) {
+								uiReferencePopoverController.hidePopover();
 
-							// TODO: verify if this is the best approach to opening modals
-							return editor.openModal({
-								controller: 'DocumentPreviewModalController',
-								templateUrl: require.toUrl('fontoxml-references-document/ui/document-preview-modal-template.html'),
-								resolve: {
-									documentRemoteId: function () {
-										return documentRemoteId;
+								// TODO: verify if this is the best approach to opening modals
+								return editor.openModal({
+									controller: 'DocumentPreviewModalController',
+									templateUrl: require.toUrl('fontoxml-references-document/ui/document-preview-modal-template.html'),
+									resolve: {
+										documentRemoteId: function () {
+											return documentRemoteId;
+										}
 									}
-								}
+								});
+							})
+							.catch(function (error) {
+								// TODO: implement error handling code
+								console.error(error);
 							});
-						})
-						.catch(function (error) {
-							// TODO: implement error handling code
-							console.error(error);
-						});
-				}
-			};
-		}
+					}
+				};
+			}
+		};
 	};
 });
