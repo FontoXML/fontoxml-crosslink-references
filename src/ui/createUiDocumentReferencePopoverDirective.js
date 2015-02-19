e7e7ae456793145e520cfc88a4623023e5f24e6c
Stef Busking <stef.busking@gmail.com>
2015-02-19 14:05:37 +0100

    Remove DITA-specific code from the package.

--- src/ui/createUiDocumentReferencePopoverDirective.js
+++ src/ui/createUiDocumentReferencePopoverDirective.js
@@ -5,28 +5,6 @@ define([
 	) {
 	'use strict';
 
-	// TODO: this kind of logic should be encapsulated in a seperate package
-	// TODO: extend to wrap the entire dita spec
-	// NOTE: could also be needed for editor flow?
-	// NOTE: we could really use a proper model for this
-	function resolveRemoteDocumentIdFromTarget (reference, nodeId) {
-		return new Promise(function (resolve, reject) {
-			// If '#' is in the target, the target is the remoteDocumentId
-			// If '#' is at the start of the target, it is a relative link to the current document
-			// Else, the part of the target up till '#' is the remoteDocumentId
-			var target = reference.target,
-				hashSymbolIndex = target.indexOf('#');
-			if (hashSymbolIndex === -1) {
-				resolve(target);
-			} else if (hashSymbolIndex === 0) {
-				// TODO: retrieve the remoteDocumentId for the document in which nodeId lives
-				reject('Not yet supported');
-			} else {
-				resolve(target.substring(0, hashSymbolIndex));
-			}
-		});
-	}
-
 	return function createUiDocumentReferencePopoverDirective () {
 		return {
 			restrict: 'E',
@@ -34,21 +12,18 @@ define([
 			scope: {},
 			require: '^uiReferencePopover',
 			link: function (scope, element, attrs, uiReferencePopoverController) {
-				// TODO: use a generic document edit operation here which has a step which determines the specific implementation
-				// according to the element being edited.
+				// TODO: use a generic document edit operation here which has a step which determines the specific
+				// implementation according to the element being edited.
 				uiReferencePopoverController.setEditOperationName('xref-dita-edit');
 
 				scope.uiDocumentReferencePopover = {
 					uiReferencePopover: uiReferencePopoverController,
 
 					previewReference: function () {
-						resolveRemoteDocumentIdFromTarget(
-							uiReferencePopoverController.reference,
-							uiReferencePopoverController.nodeId)
+						editor.getRemoteDocumentIdFromReference(uiReferencePopoverController.reference)
 							.then(function(documentRemoteId) {
 								uiReferencePopoverController.hidePopover();
 
-								// TODO: verify if this is the best approach to opening modals
 								return editor.openModal({
 									controller: 'DocumentPreviewModalController',
 									templateUrl: require.toUrl('fontoxml-references-document/ui/document-preview-modal-template.html'),
@@ -58,10 +33,6 @@ define([
 										}
 									}
 								});
-							})
-							.catch(function (error) {
-								// TODO: implement error handling code
-								console.error(error);
 							});
 					}
 				};
