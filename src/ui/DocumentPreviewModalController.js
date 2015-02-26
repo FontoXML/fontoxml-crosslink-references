df902e2788b6bef00edca2dcec22b40728fed616
Thomas Brekelmans <thomas.brekelmans@liones.nl>
2015-02-26 11:12:34 +0100

    Broken state of document preview (in preview modal). Broken styling of document preview (in preview and insert/edit modal).

--- src/ui/DocumentPreviewModalController.js
+++ src/ui/DocumentPreviewModalController.js
@@ -4,6 +4,8 @@ define([
 	'use strict';
 
 	return /* @ngInject */ function DocumentPreviewModalController ($scope, $modalInstance, reference) {
+		console.log("reference", reference);
+
 		$scope.uiDocumentPreviewModal = {
 			reference: reference,
 			targetSpec: {},
