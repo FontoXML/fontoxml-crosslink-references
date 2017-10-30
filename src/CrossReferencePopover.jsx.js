754b77eefcd8ef88b45b94afd609790f8bd29166
eva <eva.de.haas@liones.nl>
2017-10-30 14:48:10 +0100

    Add documentation to CrossReferencePopover

--- src/CrossReferencePopover.jsx.js
+++ src/CrossReferencePopover.jsx.js
@@ -58,15 +58,36 @@ const determineReferenceTextLabels = ({ target, metadata }) => {
 	};
 };
 
+/**
+ * A component used for making a popover for cross references.
+ *
+ * The CrossReferencePopover completely implements {@link FxReferencePopover} for cross referencing.
+ * The only property that still needs to be added is the `resolveReference` property.
+ *
+ * @fontosdk
+ * @category add-on/fontoxml-crosslink-references
+ */
 class CrossReferencePopover extends Component {
 	static propTypes = {
+		/**
+		 * @type {CrossReferencePopover~data}
+		 */
 		data: PropTypes.shape({
 			contextNodeId: PropTypes.string.isRequired,
 			deleteOperationName: PropTypes.string,
 			editOperationName: PropTypes.string,
 			targetIsPermanentId: PropTypes.bool,
-			targetQuery: PropTypes.string
+			targetQuery: PropTypes.string.isRequired
 		}).isRequired,
+		/**
+		 * This callback will be triggered when the popover is openend or after the permanent id is
+		 * resolved (if the reference has permanent ids `data.targetIsPermanentId`).
+		 *
+		 * @param {string} target The unresolved target, this is the resolved permanent id or
+		 * the outcome of `data.targetQuery`.
+		 *
+		 * @return {Promise.<string|Object>} The resolved target.
+		 */
 		resolveReference: PropTypes.func.isRequired
 	};
 
@@ -102,3 +123,24 @@ class CrossReferencePopover extends Component {
 }
 
 export default CrossReferencePopover;
+
+/**
+ * The popoverData as provided by the family configuration, this only needs to be passed through.
+ *
+ * @typedef   {Object}  data
+ * @memberof  CrossReferencePopover
+ * @inner
+ *
+ * @property  {NodeId}     contextNodeId               A property that comes from the popover method
+ *   self, contains the node ID of the node that is configured.
+ * @property  {string}     [deleteOperationName='reference-delete'] The operation for removing the
+ *   reference. Is by default {@link reference-delete}.
+ * @property  {boolean}    [editOperationName]         Only when an editOperationName is used, a edit
+ *   button is made. The edit operation should provide a way to edit the reference.
+ * @property  {XPathQuery} targetQuery                 Determines the reference content with a xpath
+ *   query, starting from the context node. Often this is just an attribute, for example `@href`.
+ * @property  {boolean}    [targetIsPermanentId=false] Determines wether the reference contains
+ *   permanentId's.
+ *
+ * @fontosdk  members
+ */
