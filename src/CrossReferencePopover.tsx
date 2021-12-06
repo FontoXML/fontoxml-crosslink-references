04f531c6763610e55385f5d0cad990e379f15760
wvbe <wybe@x-54.com>
2021-12-06 17:09:00 +0100

    DEV-12508 Convert JSDoc to TSDoc

--- src/CrossReferencePopover.tsx
+++ src/CrossReferencePopover.tsx
@@ -9,41 +9,44 @@ import operationsManager from 'fontoxml-operations/src/operationsManager';
 import CrossReferencePopoverBody from './ui/CrossReferencePopoverBody';
 
 /**
+ * @remarks
  * A component used for making a popover for cross references.
  *
- * The CrossReferencePopover completely implements {@link FxReferencePopover} for cross referencing.
- * The only property that still needs to be added is the `resolveReference` property.
+ * The CrossReferencePopover completely implements {@link FxReferencePopover} for
+ * cross referencing. The only property that still needs to be added is the
+ * `resolveReference` property.
  *
  * @fontosdk importable
- * @react
- * @category add-on/fontoxml-crosslink-references
  */
 const CrossReferencePopover: React.FunctionComponent<{
 	/**
-	 * The popoverData as provided by the family configuration, this only needs to be passed through.
-	 *
-	 * @type {CrossReferencePopover~data}
+	 * @remarks
+	 * The popoverData as provided by the family configuration, this only needs to be
+	 * passed through.
 	 *
 	 * @fontosdk
 	 */
 	data: {
 		/**
-		 * A property that comes from the popover method
-		 *   self, contains the node ID of the node that is configured.
+		 * @remarks
+		 * A property that comes from the popover method self, contains the node ID of the
+		 * node that is configured.
 		 *
 		 * @fontosdk
 		 */
 		contextNodeId: string;
 		/**
-		 * The operation for removing the
-		 *   reference. Is by default {@link reference-delete}.
+		 * @remarks
+		 * The operation for removing the reference. Is by default {@link
+		 * reference-delete}.
 		 *
 		 * @fontosdk
 		 */
 		deleteOperationName?: string;
 		/**
-		 * Only when an editOperationName is used, a edit
-		 *   button is made. The edit operation should provide a way to edit the reference.
+		 * @remarks
+		 * Only when an editOperationName is used, a edit button is made. The edit
+		 * operation should provide a way to edit the reference.
 		 *
 		 * @fontosdk
 		 */
@@ -53,48 +56,53 @@ const CrossReferencePopover: React.FunctionComponent<{
 		 */
 		isReadOnly?: boolean;
 		/**
-		 * Provide an alternative label for this
-		 *   reference, provided as REFERENCE_MARKUP_LABEL to the MessageFormat for the popover's description.
-		 *   If omitted, the configured markup label for the reference node is used instead.
+		 * @remarks
+		 * Provide an alternative label for this reference, provided as
+		 * REFERENCE_MARKUP_LABEL to the MessageFormat for the popover's description. If
+		 * omitted, the configured markup label for the reference node is used instead.
 		 *
 		 * @fontosdk
 		 */
 		referenceMarkupLabel?: string;
 		/**
-		 * Determines wether the reference contains
-		 *   permanentId's.
+		 * @remarks
+		 * Determines wether the reference contains permanentId's.
 		 *
 		 * @fontosdk
 		 */
 		targetIsPermanentId?: boolean;
 		/**
-		 * Determines the reference content with a xpath
-		 *   query, starting from the context node. Often this is just an attribute, for example `@href`.
+		 * @remarks
+		 * Determines the reference content with a xpath query, starting from the context
+		 * node. Often this is just an attribute, for example `@href`.
 		 *
 		 * @fontosdk
 		 */
 		targetQuery: string;
 	};
 	/**
-	 * This callback will be triggered when the popover is opened or after the permanent id is
-	 * resolved (if the reference has permanent ids `data.targetIsPermanentId`).
-	 *
-	 * @param {string} target The unresolved target, this is the resolved permanent id or
-	 *    the outcome of `data.targetQuery`.
-	 *
-	 * @return {Promise.<CrossReferencePopover~returnObject>} The resolved target. This
-	 *    should be a promise that resolves into an object.
+	 * @remarks
+	 * This callback will be triggered when the popover is opened or after the
+	 * permanent id is resolved (if the reference has permanent ids
+	 * `data.targetIsPermanentId`).
 	 *
 	 * @fontosdk
+	 *
+	 * @param target - The unresolved target, this is the resolved permanent id or the outcome of
+	 *                 `data.targetQuery`.
+	 *
+	 * @returns The resolved target. This should be a promise that resolves into an object.
 	 */
 	resolveReference(target: string): Promise<{
 		/**
+		 * @remarks
 		 * The document ID of the document that is referenced
 		 *
 		 * @fontosdk
 		 */
 		documentId: DocumentId;
 		/**
+		 * @remarks
 		 * The node ID of the node that is referenced
 		 *
 		 * @fontosdk
@@ -155,37 +163,13 @@ CrossReferencePopover.displayName = 'CrossReferencePopover';
 export default CrossReferencePopover;
 
 /**
- *
- * @typedef   {Object}  returnObject
- * @memberof  CrossReferencePopover
- * @inner
- *
- * @property  {DocumentId} documentId The document ID of the document that is referenced
- * @property  {NodeId}     [nodeId]   The node ID of the node that is referenced
- *
- * @fontosdk  members
+ * @fontosdk
  */
 
 /**
- * The popoverData as provided by the family configuration, this only needs to be passed through.
+ * @remarks
+ * The popoverData as provided by the family configuration, this only needs to be
+ * passed through.
  *
- * @typedef   {Object}  data
- * @memberof  CrossReferencePopover
- * @inner
- *
- * @property  {NodeId}     contextNodeId               A property that comes from the popover method
- *   self, contains the node ID of the node that is configured.
- * @property  {string}     [deleteOperationName='reference-delete'] The operation for removing the
- *   reference. Is by default {@link reference-delete}.
- * @property  {string}     [editOperationName]         Only when an editOperationName is used, a edit
- *   button is made. The edit operation should provide a way to edit the reference.
- * @property  {string}     [referenceMarkupLabel]      Provide an alternative label for this
- *   reference, provided as REFERENCE_MARKUP_LABEL to the MessageFormat for the popover's description.
- *   If omitted, the configured markup label for the reference node is used instead.
- * @property  {XPathQuery} targetQuery                 Determines the reference content with a xpath
- *   query, starting from the context node. Often this is just an attribute, for example `@href`.
- * @property  {boolean}    [targetIsPermanentId=false] Determines wether the reference contains
- *   permanentId's.
- *
- * @fontosdk  members
+ * @fontosdk
  */
